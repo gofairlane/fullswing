c42b6e0382e6ec0afd3741b50b4a2a3d1c3f133d
Rusty Calder <rusty@tinkrlabs.com>
2015-03-19 17:11:17 -0700

    formatting for mobile nav

--- header.php
+++ header.php
@@ -45,7 +45,19 @@
 		<div id="mobile-header" class="mobile-nav">
 
 			<nav role="mobile-navigation" >
-				<?php wp_nav_menu(array()); ?>
+				<?php wp_nav_menu(array(
+			         'container' => false,                           // remove nav container
+			         'container_class' => '',                 // class of container (should you choose to use it)
+			         'menu' => __( 'mobile', 'bonestheme' ),  // nav name
+			         'menu_class' => 'mobile-side-nav',               // adding custom nav class
+			         'theme_location' => 'main-nav',                 // where it's located in the theme
+			         'before' => '',                                 // before the menu
+		               'after' => '',                                  // after the menu
+		               'link_before' => '',                            // before each link
+		               'link_after' => '',                             // after each link
+		               'depth' => 0,                                   // limit the depth of the nav
+			         'fallback_cb' => ''                             // fallback function (if there is one)
+				)); ?>
 			</nav>
 
 		</div>
